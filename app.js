e4a44060711ff99429b148542b1451df0e32ac38
Tod <todvenus@gmail.com>
2024-02-16 16:26:30 +0800

    Last version

--- app.js
+++ app.js
@@ -1,26 +1,43 @@
-// A variable to save player's turn
 
-var activePlayer = 1;
+// A variable to save player's turn
+var activePlayer;
 
 // A variable to save player's collect score
-var scores = [0, 0, 0];
+var scores;
 
 // A variable to save player's round score
-var roundScore = 0
+var roundScore;
+
+// Dice picture
+var diceDom =(document.querySelector(".dice"));
+
+var isNewGame;
+
 
 //Start version
+function initGame(){
+
+isNewGame = true;
+
+activePlayer = 1;
+
+scores = [0, 0, 0];
+
+roundScore = 0;
+
 document.getElementById('score-1').textContent ='0';
 document.getElementById('score-2').textContent = '0';
 
 document.getElementById('current-2').textContent ='0';
 document.getElementById('current-1').textContent = '0';
-
-var diceDom =(document.querySelector(".dice"));
 diceDom.style.display = 'none';
+}
 
 function switchPlayer(){
     document.getElementById('current-' + activePlayer).textContent = 0;
+
     roundScore = 0;
+    
      if(activePlayer === 1) {
         activePlayer = 2;
      }
@@ -33,14 +50,17 @@ function switchPlayer(){
     diceDom.style.display = 'none';
 }
 
+initGame();
+
 
 //Dice play evenbt listener 'roll dice button'
 document.querySelector('.dice-roll').addEventListener
 ('click', function ()
 {
-        //link to dice img and dice math function
+    if(isNewGame){
         var diceNum = Math.floor(Math.random()*6)+1;
         diceDom.style.display = 'block';
+//link to dice img and dice math function
         diceDom.src = '/img/dice-' + diceNum + '.png';
     
     if(diceNum !== 1) {
@@ -49,14 +69,25 @@ document.querySelector('.dice-roll').addEventListener
     }
     else {
         switchPlayer();
-    }
+    };
 }
-);
+});
 // hold button
 document.querySelector('.hold').addEventListener
 ('click', function(){
-
+    if(isNewGame){
     scores[activePlayer] = scores[activePlayer] + roundScore;
-    document.getElementById('score-' + activePlayer).textContent = scores[activePlayer];
-    switchPlayer();
+
+document.getElementById('score-' + activePlayer).textContent = scores[activePlayer];
+}
+//WInner
+if(scores[activePlayer] >= 10){
+    document.getElementById('name-' + activePlayer).textContent= "Winner!!!";
+    document.querySelector('.player-' + activePlayer).classList.add('winner');
+    document.querySelector('.player-' + activePlayer).classList.remove('active');
+    isNewGame =false;
+} 
 });
+
+//New game function
+document.querySelector('.new-game').addEventListener('click', initGame);
